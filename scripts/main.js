2185519c67269db00b68082717a6f207acefa69a
Akira Shirai <akira.shirai@gmail.com>
2017-04-11 00:04:22 -0600

    slide nav in/out

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,48 @@
 $(document).ready(function()
 {
+    var rightNavWidth = 350;
+    var orderYoursWidth = 150;
+
     $('.minus').click(function() {
-
-        if($(this).css("margin-right") == "0px")
-        {
-            $('.right').animate({"margin-right": '-=200'});
-            $('.plus').animate({"margin-right": '-=200'});
-            $('.minus').animate({"margin-right": '-=200'});
-            $('.order-yours').animate({"margin-right": '+=100'});
+        if($(this).css("margin-right") == "0px") {
+            showRightSection(false);
+            showOrderYours(true);
+        }else{
+            showRightSection(true);
+            showOrderYours(false);
         }
-        else
-        {
-            $('.right').animate({"margin-right": '+=200'});
-            $('.plus').animate({"margin-right": '+=200'});
-            $('.minus').animate({"margin-right": '+=200'});
-            $('.order-yours').animate({"margin-right": '-=100'});
-        }
-
     });
 
     $('.order-yours').click(function() {
-
-        if($(this).css("margin-right") !== "0px")
-        {
-            $('.right').animate({"margin-right": '-=200'});
-            $('.plus').animate({"margin-right": '-=200'});
-            $('.minus').animate({"margin-right": '-=200'});
-            $('.order-yours').animate({"margin-right": '+=100'});
-        }
-        else
-        {
-            $('.right').animate({"margin-right": '+=200'});
-            $('.plus').animate({"margin-right": '+=200'});
-            $('.minus').animate({"margin-right": '+=200'});
-            $('.order-yours').animate({"margin-right": '-=100'});
+        if($(this).css("margin-right") !== "0px"){
+            //nothing happens here
+        }else{
+            showRightSection(true);
+            showOrderYours(false);
         }
 
     });
+
+    function showRightSection(show){
+        showRightNav(show);
+        showPlus(show);
+        showMinus(show);
+    }
+
+    function showRightNav(show){
+        $('.right').animate({"margin-right": (show ? '+' : '-') + '=' + rightNavWidth});
+    }
+
+    function showPlus(show){
+        $('.plus').animate({"margin-right": (show ? '+' : '-') + '=' + rightNavWidth});
+    }
+
+    function showMinus(show){
+        $('.minus').animate({"margin-right": (show ? '+' : '-') + '=' + rightNavWidth});
+    }
+
+    function showOrderYours(show){
+        $('.order-yours').animate({"margin-right": (show ? '+' : '-') + '=' + orderYoursWidth});
+    }
+
 });
